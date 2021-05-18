191343d8879f5e63d152353a6b84da432bcdcd28
James Fielder <james@fielder.dev>
2021-05-18 23:08:48 +0100

    await json read

--- postprocess.ts
+++ postprocess.ts
@@ -1,14 +1,14 @@
 import { htmlRead } from './html.ts';
-import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts'
+import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts';
 import { format } from "https://deno.land/std@0.91.0/datetime/mod.ts";
 
 const filename = Deno.args[0];
-const text = await Deno.readTextFile(filename)
+const text = await Deno.readTextFile(filename);
 
-console.log("Reading html into sentences")
+console.log("Reading html into sentences");
 
 const sentences: Array<string> = htmlRead(text);
-const current: Array<any> = readJSON('./sentences.json');
+const current: Array<any> = await readJSON('./sentences.json');
 
 current.push({
     sentences: sentences,
